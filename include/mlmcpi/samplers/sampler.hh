8702fca74b818ae99040d62feb8fdc8270a6ffa7
Nils Friess <nils.friess@gmail.com>
2023-05-12 22:36:43 +0200

    Add virtual destructor to abstract base class `sampler`

--- include/mlmcpi/samplers/sampler.hh
+++ include/mlmcpi/samplers/sampler.hh
@@ -6,6 +6,8 @@ template <typename Action> struct sampler {
   using PathType = typename Action::PathType;
 
   virtual std::optional<PathType> perform_step(const PathType &) = 0;
+
+  virtual ~sampler() = default;
 };
 
 } // namespace mlmcpi
