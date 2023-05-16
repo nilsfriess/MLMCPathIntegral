5ff5bed3f15eb6c05630eeeef778cd15c039c0d1
Nils Friess <nils.friess@gmail.com>
2023-05-16 11:51:21 +0200

    Implement autotuning of HMC sampler

--- include/mlmcpi/samplers/hmc.hh
+++ include/mlmcpi/samplers/hmc.hh
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <random>
 
 namespace mlmcpi {
@@ -14,8 +15,9 @@ template <typename Action, typename Engine = std::mt19937>
 struct hmc_sampler : sampler<Action> {
   using PathType = blaze::DynamicVector<double>;
 
-  hmc_sampler(std::shared_ptr<Action> action_, std::shared_ptr<Engine> engine_)
-      : action{std::move(action_)}, engine{std::move(engine_)} {}
+  hmc_sampler(double stepsize, std::shared_ptr<Action> action_,
+              std::shared_ptr<Engine> engine_)
+      : dt{stepsize}, action{std::move(action_)}, engine{std::move(engine_)} {}
 
   std::optional<PathType> perform_step(const PathType &current) override {
     auto position = current;
@@ -27,7 +29,6 @@ struct hmc_sampler : sampler<Action> {
     auto initial_kinetic = 0.5 * blaze::sqrNorm(momentum);
 
     constexpr int timesteps = 100;
-    constexpr double dt = 0.12;
     for (int k = 0; k < timesteps; ++k) {
       auto dt_momentum = dt;
       auto dt_position = dt;
@@ -60,7 +61,53 @@ struct hmc_sampler : sampler<Action> {
       return {};
   }
 
+  std::optional<double> autotune_stepsize(const PathType &initial_path,
+                                          double acceptance_rate_target = 0.8) {
+    const std::size_t n_samples = 1000;
+    const std::size_t n_repetitions = 100;
+    const double dt_initial = dt;
+
+    double dt_min = 0.1 * dt;
+    double dt_max = 10 * dt;
+
+    auto current = initial_path;
+
+    // Perform burnin
+    std::size_t n_burnin = 1000;
+    for (std::size_t i = 0; i < n_burnin; ++i) {
+      auto proposal = perform_step(current);
+      current = proposal.value_or(current);
+    }
+
+    for (std::size_t run = 0; run < n_repetitions; ++run) {
+      std::size_t accepted_samples = 0;
+
+      dt = 0.5 * (dt_min + dt_max);
+      for (std::size_t i = 0; i < n_samples; ++i) {
+        auto proposal = perform_step(current);
+        if (proposal) {
+          current = proposal.value();
+          accepted_samples++;
+        }
+      }
+
+      const auto acceptance_rate = (1. * accepted_samples) / n_samples;
+      if (acceptance_rate > acceptance_rate_target)
+        dt_min = dt;
+      else
+        dt_max = dt;
+
+      if (std::abs(acceptance_rate - acceptance_rate_target) < 1e-2)
+        return dt;
+    }
+
+    dt = dt_initial;
+    return {};
+  }
+
 private:
+  double dt;
+
   std::shared_ptr<Action> action;
 
   std::shared_ptr<Engine> engine;
